d581b54765e389dca8b7432111c6f2ff4d10b973
BloodCryHiro <jo090989coding@gmail.com>
2021-11-17 03:15:46 +0800

    Finshed Shark's basic movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import GroupSingle
 
 pygame.display.set_caption("Shark Game")
 window_surface = pygame.display.set_mode((1200, 800))
@@ -7,14 +8,31 @@ window_surface = pygame.display.set_mode((1200, 800))
 class Shark(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        # TODO: Import and setup sprite sheet
         self.image = pygame.transform.scale(
             pygame.image.load("Assets/Arts/shark.png").convert_alpha(), (200, 100))
         self.rect = self.image.get_rect(bottomleft=(600, 600))
 
+    def movement(self):
+        # TODO: Change velocity base on how big player grow
+        shark_velocity = 5
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[pygame.K_LEFT]:
+            self.rect.x -= shark_velocity
+        if pressed_key[pygame.K_RIGHT]:
+            self.rect.x += shark_velocity
+        if pressed_key[pygame.K_UP]:
+            self.rect.y -= shark_velocity
+        if pressed_key[pygame.K_DOWN]:
+            self.rect.y += shark_velocity
 
-def render(shark_group):
+    def update(self):
+        self.movement()
+
+
+def render(shark_group: GroupSingle):
     background = pygame.transform.scale(
-        pygame.image.load("Assets/Arts/background.png"), (1280, 960))
+        pygame.image.load("Assets/Arts/background.png").convert_alpha(), (1280, 960))
 
     window_surface.blit(background, (0, 800 - 960))
     shark_group.draw(window_surface)
@@ -37,6 +55,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
+        shark_group.update()
         render(shark_group)
 
 
