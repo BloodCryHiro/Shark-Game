abe208bcc304806102b2372d5c224ac89d5cad65
BloodCryHiro <jo090989coding@gmail.com>
2021-11-17 01:28:19 +0800

    Render background and shark to the screen

--- main.py
+++ main.py
@@ -4,13 +4,31 @@ pygame.display.set_caption("Shark Game")
 window_surface = pygame.display.set_mode((1200, 800))
 
 
-def render():
-    background = pygame.image.load()
+class Shark(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.transform.scale(
+            pygame.image.load("Assets/Arts/shark.png").convert_alpha(), (200, 100))
+        self.rect = self.image.get_rect(bottomleft=(600, 600))
+
+
+def render(shark_group):
+    background = pygame.transform.scale(
+        pygame.image.load("Assets/Arts/background.png"), (1280, 960))
+
+    window_surface.blit(background, (0, 800 - 960))
+    shark_group.draw(window_surface)
+
+    pygame.display.update()
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+
+    shark = Shark()
+    shark_group = pygame.sprite.GroupSingle()
+    shark_group.add(shark)
 
     while True:
         clock.tick(60)
@@ -18,6 +36,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
+        render(shark_group)
 
 
 if __name__ == "__main__":
